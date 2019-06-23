856917ecea6d730ee963e28be946dc16dcb5af8a
Your Name <you@example.com>
2019-06-23 13:54:15 -0300

    search controller

--- app/Controllers/SearchController.cs
+++ app/Controllers/SearchController.cs
@@ -2,36 +2,47 @@ using System.Collections.Generic;
 using System.Linq;
 using app.Models;
 using Microsoft.AspNetCore.Mvc;
+using OpenNLP.Tools.Chunker;
+using OpenNLP.Tools.Tokenize;
 
-namespace app.Controllers {
-    [Route ("api/search")]
-    public class SearchController : Controller {
+namespace app.Controllers
+{
+    [Route("api/search")]
+    public class SearchController : Controller
+    {
         public GlougliDbCOntext Context { get; }
 
-        public SearchController (GlougliDbCOntext context) {
+        public SearchController(GlougliDbCOntext context)
+        {
             Context = context;
         }
 
         [HttpGet()]
-        public ActionResult Get ([FromQuery]string query) {
+        public ActionResult Get([FromQuery]string query)
+        {
 
-            List<PaginaRank> paginas = new List<PaginaRank> ();
+//var sentence = new Sentence("Your text should go here");
+//var lemmas = sentence.Lemmas;
+//var pos = sentence.PosTags;
 
-            var consulta = Context.Conteudo.Where(x => x.Palavra.Contains (query));
-            
-            var paginasBd = Context.Pagina;
+            // English tokenizer
+            var tokenizer = new EnglishRuleBasedTokenizer(false);
+            var tokens = tokenizer.Tokenize(query);
 
-            paginas.AddRange(from pag in paginasBd
-                             let count = consulta.Where(x => x.Id == pag.Id).Count()
-                             select new PaginaRank()
-                             {
-                                 Titulo = pag.Titulo,
-                                 Link = pag.Link,
-                                 Avaliacao = count
-                             });
-            paginas = paginas.OrderBy (x => x.Avaliacao).ToList (); // Ordena a Lista de Páginas pela quantidade de Avaliacoes
+            var consulta = Context.Conteudo.Where(conteudo => tokens.Contains(conteudo.Palavra));
 
-            return Ok (paginas);
+            var paginas = from pag in Context.Pagina
+                          let count = consulta.Where(x => x.Id == pag.Id).Count()
+                          select new PaginaRank()
+                          {
+                              Titulo = pag.Titulo,
+                              Link = pag.Link,
+                              Avaliacao = count
+                          };
+
+            paginas = paginas.OrderBy(x => x.Avaliacao); // Ordena a Lista de Páginas pela quantidade de Avaliacoes
+
+            return Ok(paginas);
         }
     }
 }
